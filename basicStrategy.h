31e0e912dcf0b5d1b335b040a6a76d996fbaf4f6
Adrian <ahurta92@gmaio.com>
2017-01-24 18:24:23 -0500

    divided winPercent by 100 ,more intuitive for user

--- basicStrategy.h
+++ basicStrategy.h
@@ -7,7 +7,7 @@ using namespace std;
 double expected_Value(double winPercent, double potSize,double betSize){
     double WinAmount=potSize+betSize;
     
-    return winPercent*WinAmount+(1-winPercent)*(betSize);
+    return (winPercent/100)*WinAmount+(1-(winPercent/100))*(betSize);
 }
 
 
