045d86f5d5efc4992801499cebe6b3a3e8752720
Adrian <ahurta92@gmaio.com>
2017-01-24 18:45:55 -0500

    Added pot odds function

--- basicStrategy.cpp
+++ basicStrategy.cpp
@@ -6,15 +6,24 @@
 using namespace std;
 
 int main(void){
-
-    double EV,betSize,potSize,winPercentage;
+    bool c; //continue
+    do{
+    double EV,betAmount,potSize,winPercentage,potOdds;
     cout<<"Pot Size : $";
     cin >>potSize;
-    cout<<endl<<"bet Size : $";
-    cin>>betSize;
+    cout<<endl<<"bet amount : $";
+    cin>>betAmount;
     cout<<endl<<"Win Percentage : %";
     cin>>winPercentage;
 
-    EV=expected_Value(winPercentage,potSize,betSize);
+    EV=expected_Value(winPercentage,potSize,betAmount);
+    potOdds=Pot_Odds(betAmount,potSize);
+
     cout << "Expected Value = $" << EV <<endl;
+    cout << "Pot Odds = %"<<potOdds<<endl;
+
+
+    cout<<"Again? 1=yes 0=no";cin>>c;
+
+    }while(c);//continue?
 }
--- basicStrategy.h
+++ basicStrategy.h
@@ -3,11 +3,22 @@
 #include <iomanip> 
 
 using namespace std;
-
-double expected_Value(double winPercent, double potSize,double betSize){
-    double WinAmount=potSize+betSize;
+/**
+Expected Value is the probability-weighted average of possible results
+EV = Win% * WinAmt - Lose% * LoseAmt
+*/
+double expected_Value(double winPercent, double potSize,double betAmount){
+    double WinAmount=potSize+betAmount;
     
-    return (winPercent/100)*WinAmount+(1-(winPercent/100))*(betSize);
+    return (winPercent/100)*WinAmount+(1-(winPercent/100))*(betAmount);
+}
+/**
+Pot Odds are the relationship of the call amount to the size of the pot
+
+*/
+double Pot_Odds(double betAmount, double potSize ){
+    return betAmount/(betAmount+betAmount+potSize);
+
 }
 
 
