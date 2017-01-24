48053df685c9fa331920a52e4cbe2198349d5621
Adrian <ahurta92@gmaio.com>
2017-01-24 18:08:47 -0500

    added Expected Value Function to main

--- basicStrategy.cpp
+++ basicStrategy.cpp
@@ -1,6 +1,17 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip> 
-#include <basicstrategy.h>
+#include <basicStrategy.h>
 
 using namespace std;
+
+int main(void){
+
+    double EV,betSize,potSize,WinPercentage;
+    cout<<"Pot Size : ";
+    cin >>potSize;
+    cout<<endl<<"bet Size ";
+    cin>>betSize;
+
+    EV=expected_Value()
+}
